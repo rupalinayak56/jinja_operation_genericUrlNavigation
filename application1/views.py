8183a6a8f6ad634389474e2b253db01702eb4222
rupalinayak56 <rupalinayak7789@gmail.com>
2024-06-28 11:19:24 +0530

    first commit

---
+++ application1/views.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render
+
+# Create your views here.
+def rupali(request):
+    return render(request,'rupali.html')
+def jyoti(request):
+    return render(request,'jyoti.html')
